6892fffadbcac0e0b0fe29d1a0b7b25bc113d1e1
aiiiden0 <hjjam100@gmail.com>
2025-01-19 11:38:31 +0900

    chore: replace font family

--- packages/mini-app/src/provider/react-95.tsx
+++ packages/mini-app/src/provider/react-95.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { styleReset } from 'react95';
-import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
-import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
 import original from 'react95/dist/themes/original';
 import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
@@ -13,19 +11,17 @@ const GlobalStyles = createGlobalStyle`
   }
 
   @font-face {
-    font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif}') format('woff2');
-    font-weight: 400;
-    font-style: normal
+    font-family: 'UnifontExMono';
+    src: url('https://stgiga.github.io/UnifontEX/UnifontExMono.eot');
+    src: url('https://stgiga.github.io/UnifontEX/UnifontExMono.eot?#iefix') format('embedded-opentype'),
+      url('https://stgiga.github.io/UnifontEX/UnifontExMono.woff') format('woff'),
+      url('https://stgiga.github.io/UnifontEX/UnifontExMono.ttf') format('truetype'),
+      url('https://stgiga.github.io/UnifontEX/UnifontExMono.svg#UnifontExMono') format('svg');
+    font-weight: normal;
   }
-  @font-face {
-    font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif_bold}') format('woff2');
-    font-weight: bold;
-    font-style: normal
-  }
+
   body, input, select, textarea {
-    font-family: 'ms_sans_serif';
+    font-family: "UnifontExMono", sans-serif;
   }
 
   html {
