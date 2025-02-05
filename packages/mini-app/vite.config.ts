ea8af3f7e3a618cd4ad80c939df6ec856095e9a7
aiiiden0 <hjjam100@gmail.com>
2025-02-05 14:11:50 +0900

    chore: add million dev

--- packages/mini-app/vite.config.ts
+++ packages/mini-app/vite.config.ts
@@ -1,10 +1,16 @@
+import MillionLint from '@million/lint';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    MillionLint.vite({
+      enabled: true,
+    }),
+    react(),
+  ],
   server: {
     port: 3000,
   },
